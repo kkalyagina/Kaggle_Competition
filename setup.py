1e5198f2066b08b5373d2bff35f27b2e8484da56
Kristina Kaliagina <76058935+kkalyagina@users.noreply.github.com>
2021-04-29 19:46:18 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setuptools.setup(
-    name="utils_project",
+    name="project_2month",
     version="0.0.1",
     author="Kristina Kaliagina",
     author_email="Kristina_Kaliagina@epam.com",
-    description="Project_2month",
+    description="EPAM project",
     url="https://github.com/kkalyagina/epam.git",
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
